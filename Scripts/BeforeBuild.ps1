8ed068e3a5aee3489d5e0e25875d86088f19bd0a
craftwar <craftwar@users.noreply.github.com>
2019-09-28 20:45:41 +0800

    Update vc_redist to VS2019

--- Scripts/BeforeBuild.ps1
+++ Scripts/BeforeBuild.ps1
@@ -6,7 +6,7 @@ Copy-Item cacert.pem 'Combo\Release\PCMan Combo\'
 Copy-Item cacert.pem 'Lite\Debug\PCMan\'
 Copy-Item cacert.pem 'Lite\Release\PCMan\'
 
-Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?LinkId=746571' -Out 'vc_redist.x86.exe'
+Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vc_redist.x86.exe' -Out 'vc_redist.x86.exe'
 
 Copy-Item 'OpenSourceLicenses.txt' 'Combo\Debug\PCMan Combo\'
 Copy-Item 'OpenSourceLicenses.txt' 'Combo\Release\PCMan Combo\'
